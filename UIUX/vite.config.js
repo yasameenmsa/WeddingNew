c3db976ed31af4afeeeb8093e04eb164840dbb9f
Yasmeen Awawdeh (Yasmin MSA) <131165262+yasameenmsa@users.noreply.github.com>
2024-08-31 15:49:52 +0300

    Initial commit 3

--- UIUX/vite.config.js
+++ UIUX/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure this is set correctly
+  root: '.', // Ensure this points to the root directory
   build: {
     outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: {
+        main: '/index.html', // Adjust path if necessary
+      },
+    },
   },
 });
